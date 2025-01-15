0752f6d9523fd3b5ba1549b84d578211b36793ac
Xu Wu <wuxu92@outlook.com>
2025-01-15 14:03:41 +0800

    update azurefleet sdk

--- resource-manager/azurefleet/2024-11-01/fleets/model_virtualmachinescalesetosprofile.go
+++ resource-manager/azurefleet/2024-11-01/fleets/model_virtualmachinescalesetosprofile.go
@@ -1,5 +1,11 @@
 package fleets
 
+import (
+	"encoding/json"
+
+	"github.com/hashicorp/go-azure-sdk/sdk/nullable"
+)
+
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See NOTICE.txt in the project root for license information.
 
@@ -9,8 +15,14 @@ type VirtualMachineScaleSetOSProfile struct {
 	AllowExtensionOperations    *bool                 `json:"allowExtensionOperations,omitempty"`
 	ComputerNamePrefix          *string               `json:"computerNamePrefix,omitempty"`
 	CustomData                  *string               `json:"customData,omitempty"`
-	LinuxConfiguration          *LinuxConfiguration   `json:"linuxConfiguration,omitempty"`
+	LinuxConfiguration          *LinuxConfiguration   `json:"linuxConfiguration"`
 	RequireGuestProvisionSignal *bool                 `json:"requireGuestProvisionSignal,omitempty"`
 	Secrets                     *[]VaultSecretGroup   `json:"secrets,omitempty"`
-	WindowsConfiguration        *WindowsConfiguration `json:"windowsConfiguration,omitempty"`
+	WindowsConfiguration        *WindowsConfiguration `json:"windowsConfiguration"`
+}
+
+var _ json.Marshaler = VirtualMachineScaleSetOSProfile{}
+
+func (s VirtualMachineScaleSetOSProfile) MarshalJSON() ([]byte, error) {
+	return nullable.MarshalNullableStruct(s)
 }
